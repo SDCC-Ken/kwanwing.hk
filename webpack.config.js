ec7a976637995de5f40989226280d05b569a03c0
ken <wingwingchan2002@gmail.com>
2018-01-07 12:34:00 +0800

    fix webpack

--- webpack.config.js
+++ webpack.config.js
@@ -4,8 +4,8 @@ var webpack = require('webpack')
 module.exports = {
   entry: './src/main.js',
   output: {
-    path: path.resolve(__dirname, './dist'),
-    publicPath: '/dist/',
+    path: path.resolve(__dirname, './public_html'),
+    publicPath: '/public_html/',
     filename: 'build.js'
   },
   resolve: {
